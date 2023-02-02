82c2ac92746e570074b671e1e6a1675e7dd51fb9
terell-davis <terellbdavis@gmail.com>
2023-02-01 21:13:32 -0500

    Removed LeaveCommand and incorporated it into EndCommand

--- src/main/java/com/ontrexdex/trexbot/commands/music/control/EndCommand.java
+++ src/main/java/com/ontrexdex/trexbot/commands/music/control/EndCommand.java
@@ -6,6 +6,9 @@ import com.ontrexdex.trexbot.commands.ICommand;
 import com.ontrexdex.trexbot.commands.music.musicassets.GuildMusicManager;
 import com.ontrexdex.trexbot.commands.music.musicassets.PlayerManager;
 import me.duncte123.botcommons.messaging.EmbedUtils;
+import net.dv8tion.jda.api.entities.AudioChannel;
+import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.managers.AudioManager;
 
 import java.util.Arrays;
 import java.util.List;
@@ -15,9 +18,23 @@ public class EndCommand implements ICommand {
     public void handle(CommandContext ctx) {
         PlayerManager playerManager = PlayerManager.getInstance();
         GuildMusicManager musicManager = playerManager.getGuildMusicManager(ctx.getGuild());
+        final TextChannel channel = ctx.getMessage().getChannel().asTextChannel();
+        AudioManager audioManager = ctx.getGuild().getAudioManager();
 
-        LeaveCommand leave = new LeaveCommand();
-        leave.handle(ctx);
+        if (!audioManager.isConnected()) {
+            channel.sendMessage("Bot is currently not in a voice channel!").queue();
+            return;
+        }
+        AudioChannel audioChannel = audioManager.getConnectedChannel();
+
+        if (!audioChannel.getMembers().contains(ctx.getMember())) {
+            channel.sendMessage("You have to be in the same voice channel as me to use this command").queue();
+            return;
+        }
+
+        audioManager.closeAudioConnection();
+        channel.sendMessage("Disconnected from your channel").queue();
+
         musicManager.scheduler.getQueue().clear();
         musicManager.player.stopTrack();
         musicManager.player.setPaused(false);
