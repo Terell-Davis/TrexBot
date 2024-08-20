0071f2b5c294b7f9dcaf0ab7aef47832081bab98
Terell-Davis <54454563+Terell-Davis@users.noreply.github.com>
2024-08-20 16:45:18 -0400

    While I was here wanted to add more variety

--- src/main/java/com/ontrexdex/trexbot/commands/RepostCommand.java
+++ src/main/java/com/ontrexdex/trexbot/commands/RepostCommand.java
@@ -5,15 +5,20 @@ import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import net.dv8tion.jda.api.utils.FileUpload;
 
 import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 
 public class RepostCommand implements ICommand {
     @Override
     public void handle(CommandContext ctx) {
         TextChannel channel = ctx.getMessage().getChannel().asTextChannel();
         String path = Config.get("ASSETS");
-        FileUpload file = FileUpload.fromData(new File(path + "repost.png"));
 
+        List<String> repost = Arrays.asList("repost.gif", "ffxivrepost.gif", "mmeyes.jpg", "suavarepost.gif");
+        Random ran = new Random();
 
+        FileUpload file = FileUpload.fromData(new File(path + repost.get(ran.nextInt(repost.size()))));
         channel.sendFiles(file).queue();
     }
 
